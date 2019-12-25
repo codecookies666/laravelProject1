cebabac1ee823e5fb465a7a28ac54742b7150890
I Minds <codecookies@163.com>
2019-12-25 16:01:47 +0000

    用户显示页面

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,4 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 
 //注册
 Route::get('signup', 'UsersController@create')->name('signup');
+Route::resource('users','UsersController');
