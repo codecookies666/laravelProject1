da2a95daa5e04816b611557a1674e194769f333f
I Minds <codecookies@163.com>
2019-12-26 05:34:26 +0000

    中文的错误提示信息

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -9,8 +9,10 @@
             </div>
             <div class="card-body">
 
+                @include('shared._errors')
+
                 <form method="POST" action="{{ route('users.store') }}">
-                    {{  csrf_field()  }}
+                    {{ csrf_field() }}
 
                     <div class="form-group">
                         <label for="name">名称：</label>
