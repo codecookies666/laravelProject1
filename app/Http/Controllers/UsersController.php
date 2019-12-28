32bf23621614c81d6f1b5e9a84f66683b701bea9
I Minds <codecookies@163.com>
2019-12-28 10:49:31 +0000

    移除邮件发送的from选项

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -107,7 +107,7 @@ class UsersController extends Controller
         $subject = '感谢注册 Weibo 应用！请确认您的邮箱。';
 
         Mail::send($view, $data, function($message) use ($from, $name, $to, $subject){
-            $message->from($from, $name)->to($to)->subject($subject);
+            $message->to($to)->subject($subject);
         });
     }
     //邮箱激活操作
