dcb53de6b0e090050ad218feb7a19622969da5d5
I Minds <codecookies@163.com>
2020-01-03 09:43:06 +0000

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -64,4 +64,37 @@ class User extends Authenticatable
         return $this->statuses()
             ->orderBy('created_at', 'desc');
     }
+
+    //获取关注的人和粉丝列表
+    public function followers()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'user_id', 'follower_id');
+    }
+    public function followings()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'follower_id', 'user_id');
+    }
+
+    //关注和取消关注的动作
+    public function follow($user_ids)
+    {
+        if (!is_array($user_ids)){
+            $user_ids = compact('user_ids');
+        }
+
+        $this->followings()->sync($user_ids, false);
+    }
+    public function unfollow($user_ids){
+        if (!is_array($user_ids)){
+            $user_ids = compact('user_ids');
+        }
+
+        $this->followings()->detach($user_ids);
+    }
+
+    //是否已关注某用户
+    public function isFollowing($user_id)
+    {
+        return $this->followings->contains($user_id);
+    }
 }
